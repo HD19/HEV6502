1cf8163da575032dd263734044608204c3e23c7b
heavy <heavy@ubuntu.(none)>
2012-10-14 19:04:31 -0700

    Updated some CPU structure

--- source/cpu/cpu.cpp
+++ source/cpu/cpu.cpp
@@ -3,4 +3,39 @@
  * CPU.CPP
  * CPU function definitions
  **********************/
- 
+
+int CPU::adci(byte* in, byte* out)
+{
+    //add with carry immediate
+    
+    
+    return 2;
+}
+int CPU::adcz(byte* in, byte* out)
+}
+
+}
+int CPU::adczx(byte* in, byte* out)
+{
+
+}
+int CPU::adca(byte* in, byte* out)
+{
+
+}
+int CPU::adcax(byte* in, byte* out)
+{
+
+}
+int CPU::adcay(byte* in, byte* out)
+{
+
+}
+int CPU::adcix(byte* in, byte* out)
+{
+
+}
+int CPU::adciy(byte* in, byte* out)
+{
+
+}
--- source/cpu/cpu.h
+++ source/cpu/cpu.h
@@ -3,8 +3,18 @@
  * CPU.H 
  * Defines CPU class for HEV6502
  **************************/
+#ifndef CPU_H
+#define CPU_H
 #include "mem.h"
 #include "../common/common.h"
+
+#define FLAG_CARRY 1 << 0 //Cary flag, used if a borrowed is required in subtraction, also used in shift and rotates
+#define FLAG_ZERO  1 << 1 //Zero flag, if op result is 0
+#define FLAG_INT   1 << 2 //Interrupt flag, they're disabled if this is set
+#define FLAG_DEC   1 << 3 //Shouldn't be used
+#define FLAG_BRK   1 << 4 //Break flag, executing a software interrupt
+#define FLAG_OVER  1 << 6 //Overflow flag, used when arithmetic op produces result too large to be represented in a byte. (> 255)
+#define FLAG_SIGN  1 << 7 //Set if result of an operation is negative, clear if positive.
  
  class CPU
  {
@@ -14,14 +24,46 @@
        byte Y; // Y register
        byte A; // Accumulator
        byte SP; // Stack pointer
-       byte ST; //Status register
+       byte ST; // Status Register
        short PC; // Program Counter
        Memory cpuMem; //CPU's memory, only a block for now.
        byte   program[0x10000]; //Program buffer. To be loaded by wrapper.
        
+       /* ADC */
+       int adci(byte* in, byte* out);
+       int adcz(byte* in, byte* out);
+       int adczx(byte* in, byte* out);
+       int adca(byte* in, byte* out);
+       int adcax(byte* in, byte* out);
+       int adcay(byte* in, byte* out);
+       int adcix(byte* in, byte* out);
+       int adciy(byte* in, byte* out);
        //Opcode Table
+       //Format will be int opFunc(byte* in, byte* out)
+       //Return type is the number of cycles, in is input, out is output that might be needed. 
        static const opTable[0x100] = {
-//     0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09  0xA0  0xB0  0xC0  0xD0  0xE0  0xF0
+//     0x0   0x1    0x2  0x3   0x4   0x5   0x6   0x7   0x8   0x9   0xA   0xB   0xC   0xD   0xE   0xF
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x00
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x10
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x20
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x30
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x40
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x50
+       NULL, &CPU::adcix, NULL, NULL, NULL, &CPU::adcz, NULL, NULL, NULL, &CPU::adci, NULL, &CPU::adca, NULL, NULL, NULL, NULL, //0x60
+       NULL, &CPU::adciy, NULL, NULL, NULL, &CPU::adczx, NULL, NULL, NULL, NULL, NULL, NULL, &CPU::adcax, NULL, NULL, NULL, //0x70
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x80
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x90
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xA0
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xB0
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xC0
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xD0
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xE0
+       NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xF0
+       }
+
+/*Not sure this is neccessary.       
+       static const timeTable[0x100] = {
+//     0x0   0x1    0x2  0x3   0x4   0x5   0x6   0x7   0x8   0x9   0xA   0xB   0xC   0xD   0xE   0xF       
        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x00
        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x10
        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0x20
@@ -38,4 +80,6 @@
        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xD0
        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xE0
        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, //0xF0
-  
+*/
+
+#endif
